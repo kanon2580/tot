2ecbf962ee544295d474334888dbe1dc367cd650
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 19:36:57 +0900

    team修正

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,7 @@ Rails.application.routes.draw do
   resources :team_members, only: [:new, :create]
 
   # teams
-  resources :team, only: [:show] do
+  resources :teams, only: [:show] do
   # teams/users
     resources :users, only: [:index, :show] do
       resources :comments, only: [:index]
