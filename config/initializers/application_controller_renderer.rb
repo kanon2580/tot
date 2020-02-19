544bcb1fce182cf296743f96737a421dfea8343f
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 18:04:34 +0900

    edit profilr実装

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '107d1e77d4f96ce805bfa344bf482fb9a551430d4df3340858147ec0e17c55f90d53fdcfe3c5295de92b2312d9d5edb3e2bca7000aecba122c7d223440bfd8be'
