5cdb9b6c6e25d48df66bad1ecf03305ea123a105
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 14:20:53 +0900

    comments table 設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_051238) do
+ActiveRecord::Schema.define(version: 2020_02_19_051702) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "issue_id", null: false
+    t.text "comment", null: false
+    t.boolean "has_best_answer", default: false, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "issues", force: :cascade do |t|
     t.integer "user_id", null: false
