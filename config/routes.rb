c83472a450fbf2fd7f13189148336b6e0db88fc3
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 15:10:15 +0900

    route 設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,38 @@
 Rails.application.routes.draw do
-  get 'comments/create'
-  get 'comments/edit'
-  get 'comments/update'
-  get 'comments/destroy'
-  get 'issues/new'
-  get 'issues/create'
-  get 'issues/index'
-  get 'issues/show'
-  get 'issues/edit'
-  get 'issues/update'
-  get 'issues/destroy'
-  get 'issues/choice'
-  get 'issues/confirm'
-  get 'issues/update_status'
-  get 'tags/index'
-  get 'tags/create'
-  get 'teams/show'
-  get 'team_members/new'
-  get 'team_members/create'
-  get 'users/my_page'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/index'
-  get 'users/show'
-  get 'home/top'
-  get 'home/about'
+
+  # home
+  root 'home#top'
+  get '/about' => 'home#about'
+
+  # users
   devise_for :users
+  resources :users, only: [:edit, :update] do
+    resources :comments, only: [:index]
+    resources :issues, only: [:index]
+  end
+  get 'users/:id' => 'users#my_page'
+
+  # team_members
+  resources :tesm_members, only: [:new, :create]
+
+  # teams
+  resources :team, only: [:show] do
+  # teams/users
+    resources :users, only: [:index, :show] do
+      resources :comments, only: [:index]
+      resources :issues, only: [:index]
+    end
+  # teams/tags
+    resources :tags, only: [:index, :create]
+  # teams/issues
+    resources :issue do
+      resources :comments, only: [:create, :edit, :update, :destroy]
+    end
+    get '/issues/:id/choice' => 'issues#choice'
+    get '/issues/:id/confirm' => 'issues#confirm'
+    put 'issues/:id/settled' => 'issues#settled'
+
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
