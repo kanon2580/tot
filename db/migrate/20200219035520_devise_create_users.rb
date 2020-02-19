5c176e0310d3f58f4511ab87afdea86f28598bbd
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 12:59:39 +0900

    devise db設定

--- db/migrate/20200219035520_devise_create_users.rb
+++ db/migrate/20200219035520_devise_create_users.rb
@@ -32,6 +32,9 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :name
+      t.string :profile_image_id
+      t.text :introduction
 
       t.timestamps null: false
     end
