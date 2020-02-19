a9d902f7ef213e6288d4d176c190aefeb48fca4b
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 13:11:01 +0900

    team_members table 設定

--- db/migrate/20200219040808_create_team_members.rb
+++ db/migrate/20200219040808_create_team_members.rb
@@ -1,8 +1,11 @@
 class CreateTeamMembers < ActiveRecord::Migration[5.2]
   def change
     create_table :team_members do |t|
+      t.integer :user_id
+      t.integer :team_id
 
       t.timestamps
     end
+    add_index :team_members, [:user_id, :team_id], unique: true
   end
 end
