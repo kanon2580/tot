8fa1c0a0e32a2c988583d1faddc1d3d09d81d938
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-19 13:18:19 +0900

    taggings table 設定

--- db/migrate/20200219041605_create_taggings.rb
+++ db/migrate/20200219041605_create_taggings.rb
@@ -1,8 +1,11 @@
 class CreateTaggings < ActiveRecord::Migration[5.2]
   def change
     create_table :taggings do |t|
+      t.integer :tag_id
+      t.integer :issue_id
 
       t.timestamps
     end
+    add_index :taggings, [:tag_id, :issue_id], unique: true
   end
 end
