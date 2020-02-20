d2691daf6c4383d6b997d46c9f7aa15836d5a006
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-20 10:39:59 +0900

    comment 新規投稿フォーム実装

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,12 @@
 class CommentsController < ApplicationController
   def create
+    comment = Comment.new(comment_params)
+    comment.user = current_user
+    comment.issue_id = params[:issue_id]
+    unless comment.save
+      flash[:error] = "your comment had not save :("
+    end
+    redirect_back(fallback_location: root_path)
   end
 
   def edit
@@ -9,5 +16,10 @@ class CommentsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:user_id, :issue_id, :comment)
   end
 end
--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -20,6 +20,7 @@ class IssuesController < ApplicationController
 
   def show
     @issue = Issue.find(params[:id])
+    @comment = Comment.new
   end
 
   def edit
