76e940d89d33ddbbbacdcfbe81bf6d48ccfc37b6
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-20 09:45:53 +0900

    issue新規作成フォーム実装

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,8 +1,18 @@
 class IssuesController < ApplicationController
   def new
+    @issue = Issue.new
   end
 
   def create
+    @issue = Issue.new(issue_params)
+    issue.team_id = params[:team_id]
+    issue.user_id = current_user.id
+    if issue.save
+      redirect_to team_issue_path(@issue)
+    else
+      flash[:error] = "we couldn't save your issue :("
+      redirect_back(fallback_location: root_path)
+    end
   end
 
   def index
@@ -27,5 +37,10 @@ class IssuesController < ApplicationController
   end
 
   def update_status
+  end
+
+  private
+  def issue_params
+    params.require(:issue).permit(:user_id, :team_id, :body, :has_settled, :settled_at)
   end
 end
