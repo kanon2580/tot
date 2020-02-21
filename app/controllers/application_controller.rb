79fbc93d61e87949304c02f47143a2cc5fcfb4bd
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-21 21:47:32 +0900

    ルーティング変更に合わせて修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    mypage_path(current_user)
   end
 
   def set_team
