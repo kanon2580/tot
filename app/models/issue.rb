2a0acede11b86b341a50593eb2c50512c22cb1c3
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-25 20:14:19 +0900

    model設定修正

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,9 +2,10 @@ class Issue < ApplicationRecord
 	validates	:title, presence: true
   validates	:body, presence: true
 
-  has_many :with_tags, dependent: :destroy
 	has_many :likes, dependent: :destroy
 	has_many :comments, dependent: :destroy
+	has_many :taggings
+	has_many :tags, through: :taggings
 	has_many :response_evaluations, dependent: :destroy
 	has_many :required_time_evaluations, dependent: :destroy
 
