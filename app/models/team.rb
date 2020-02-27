fe13e2e7bf08c72d982f80713fcc27b3f30c9eb5
kanon2580 <kanon.ueno1117@gmail.com>
2020-02-27 18:08:57 +0900

    model設定修正

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,6 +2,7 @@ class Team < ApplicationRecord
   validates	:name, presence: true
 
   has_many :team_members, dependent: :destroy
+  has_many :users, through: :team_members
 	has_many :issues, dependent: :destroy
 	has_many :tags, dependent: :destroy
 end
