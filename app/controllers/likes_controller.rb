047cdb1d1a37fdadc830b39dbc652f5d6e05dc82
kanon2580 <kanon.ueno1117@gmail.com>
2020-03-12 21:39:54 +0900

    [add]いいねAjax対応

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,13 +4,11 @@ class LikesController < ApplicationController
   def create
     like = current_user.likes.new(issue_id: @issue.id)
     like.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     like = @issue.likes.find_by(user_id: current_user)
     like.destroy
-    redirect_back(fallback_location: root_path)
   end
 
   private
